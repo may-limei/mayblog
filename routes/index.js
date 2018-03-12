5c0de8ff9a193770a8db4c8128487a6b572af9d1
may-limei <limei_wu@yeah.net>
2018-03-12 15:58:12 +0800

    首页侧边栏更新

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+  res.render('index', { title: '首页' });
+});
+
+/* GET home page. */
+router.get('/index.html', function(req, res, next) {
+  res.render('index', { title: '首页' });
+});
+
+/* GET generic page. */
+router.get('/generic.html', function(req, res, next) {
+  res.render('generic.html', { title: 'generic' });
 });
 
 module.exports = router;
